9988ca0a221625a1d2eb560f4b0b0fe48c5bfc2e
Vikas <vikas1618072@gmail.com>
2023-05-30 12:18:20 +0530

    add(postgres, etc): :art: add postgres

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6h(eu=p#*ucdk37p!ds_xhd+48uqge^9sh0l7o)hvx7hk*@k$3'
+SECRET_KEY = config('SECRET_KEY', 'django-insecure-6h(eu=p#*ucdk37p!ds_xhd+48uqge^9sh0l7o)hvx7hk*@k$3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,13 +75,24 @@ WSGI_APPLICATION = 'twitter_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DATABASE_NAME'),
+            'HOST': config('DATABASE_HOST'),
+            'PASSWORD': config('DATABASE_PASSWORD'),
+            'PORT': config('DATABASE_PORT'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
